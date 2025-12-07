64d0a5cd1caba3f9fc511b2dd793c8aeb55d2d30
luisKlap <luis_klap@outlook.com>
2025-12-07 15:50:16 -0300

    feat: authentication home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { Signup } from './auth/components/signup/signup';
-import { Auth } from './auth/auth';
 import { Login } from './auth/components/login/login';
+import { Auth } from './auth/auth';
+import { AuthGuard } from './auth/guards/auth.guard';
 
 export const routes: Routes = [
   { path: 'signup', component: Signup },
@@ -14,10 +15,10 @@ export const routes: Routes = [
 
   {
     path: 'home',
+    canActivate: [AuthGuard],
     loadComponent: () => import('./home/home').then(m => m.Home)
   },
 
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login' }
 ];
-
